148acff69e689d5b525d81b100e5bc670293da96
Jobo-ds <80695325+Jobo-ds@users.noreply.github.com>
2022-01-20 23:09:02 +0100

    Move

---
+++ main.py
@@ -0,0 +1,105 @@
+"""
+Imports
+"""
+import os
+from os import path
+import random
+import shutil
+import hashlib
+import cv2
+import math
+
+
+from video import *
+
+"""
+Setup Process of file
+"""
+
+print("Put the input file into the input folder.")
+
+GUI_cmd = False
+
+file = "test_files/test_long_file.mp4"
+if GUI_cmd: file = input("Enter Filename: ")
+file_type = file[-4:]
+
+# Assign ID to file (md5 Hash, for now)
+file_id = str(hashlib.md5(file.encode()).hexdigest())
+
+# Create folders
+input_folder = "input/" + file_id
+if not path.exists(input_folder):
+    os.makedirs(input_folder, exist_ok=False)
+checkpoint_folder = input_folder + "/" + "checkpoints"
+if not path.exists(checkpoint_folder):
+    os.makedirs(checkpoint_folder, exist_ok=False)
+output_folder = "output/" + file_id
+if not path.exists(output_folder):
+    os.makedirs(output_folder, exist_ok=False)
+
+# Prepare Files
+shutil.copy("input/" + file, input_folder + "/" + file_id + file_type)
+file = input_folder + "/" + file_id + file_type
+main_json_file = input_folder + "/" + file_id + ".json"
+
+# Create Main File JSON
+if not path.exists(main_json_file):
+    with open(main_json_file, 'w', encoding='utf-8') as outfile:
+        json_dict = mainJson(file, input_folder)
+        json.dump(json_dict, outfile, indent=4, ensure_ascii=False)
+
+file_info = loadJSON(main_json_file)
+
+"""
+Create Checkpoints
+"""
+
+# Calculate number of checkpoints
+if file_info["checkpoints"] == 0:
+    if file_info["duration"] <= 120: # 2 minutes
+        updateJSON(main_json_file, "checkpoints", 1)
+        print("Very short file detected (< 1 min.). Only using 1 checkpoint.")
+    elif file_info["duration"] <= 600: # 10 minutes
+        avg_checkpointDuration = 120
+        checkpoints = math.ceil(file_info["duration"] / avg_checkpointDuration)
+        updateJSON(main_json_file, "checkpoints", checkpoints)
+        updateJSON(main_json_file, "avg checkpoint duration", 120)
+        print(f"Short file detected (< 10 min.). Using {checkpoints} checkpoints.")
+    else:
+        avg_checkpointDuration = 300
+        checkpoints = math.ceil(file_info["duration"] / avg_checkpointDuration)
+        updateJSON(main_json_file, "checkpoints", checkpoints)
+        updateJSON(main_json_file, "avg checkpoint duration", 300)
+        print(f"Long file detected (> 10 min.). Using {checkpoints} checkpoints.")
+
+file_info = loadJSON(main_json_file)
+#%%
+# Create Checkpoint JSONs
+if file_info["checkpoints JSON"] == False:
+    i = 0
+    while i < file_info["checkpoints"]:
+        checkpoint_number = str(i).zfill(3)
+        checkpoint_json_file = checkpoint_folder + "/" + checkpoint_number + ".json"
+        with open(checkpoint_json_file, 'w', encoding='utf-8') as outfile:
+            json_dict = checkpointJson()
+            json.dump(json_dict, outfile, indent=4, ensure_ascii=False)
+        i = i + 1
+
+# Update main JSON
+updateJSON(main_json_file, "checkpoints JSON", True)
+
+"""
+Split file into parts
+"""
+
+splitvideo(file, main_json_file)
+
+# Process Checkpoints
+
+# Merge Checkpoints
+
+# Quality Testing
+
+# Test removal
+shutil.rmtree(input_folder)
---
+++ video.py
@@ -0,0 +1,147 @@
+"""
+Imports & Setup
+
+"""
+import json
+import ffmpeg
+
+"""
+Utilities
+"""
+
+"""
+Create Main file JSON
+ - File Integrity Details
+    - File length
+    - File Size
+    - Video codec
+    - Frames Per Second
+    - Audio codec
+    - Bitrate
+ - Process booleans allow for start/stopping process:
+        One boolean for each Checkpoint.
+"""
+
+
+def mainJson(file, input_folder):
+    probe = ffmpeg.probe(file)
+
+    json_dict = {
+        "duration": float(probe["streams"][0]["duration"]),
+        "file size": int(probe["format"]["size"]),
+        "resolution": str(probe["streams"][0]["width"]) + "x" + str(probe["streams"][0]["height"]),
+        "aspect ratio": str(probe["streams"][0]["display_aspect_ratio"]),
+        "checkpoints": 0,
+        "checkpoints JSON": False,
+        "avg checkpoint duration" : 0,
+        "checkpoints completed": [],
+        "vCodec": str(probe["streams"][0]["codec_name"]),
+        "vBitrate": int(probe["streams"][0]["bit_rate"]),
+        "FPS": str(probe["streams"][0]["r_frame_rate"]),
+        "aCodec": str(probe["streams"][1]["codec_name"]),
+        "sample rate": int(probe["streams"][1]["sample_rate"]),
+        "Bitrate": int(probe["streams"][1]["bit_rate"]),
+        "folder": str(input_folder),
+        "file_id": str(input_folder[6:])
+    }
+    return json_dict
+
+
+def loadJSON(main_json_file):
+    with open(main_json_file, "r") as content:
+        json_object = json.load(content)
+    return json_object
+
+
+def updateJSON(file, variable, value):
+    input_file = open(file, "r")
+    json_object = json.load(input_file)
+    input_file.close()
+    json_object[variable] = value
+    output_file = open(file, "w")
+    json.dump(json_object, output_file, indent=4, ensure_ascii=False)
+    output_file.close()
+
+
+"""
+Create Checkpoint JSON
+ - Process booleans allow for start/stopping process:
+            split video = False
+            split frames = False,
+            clean = false
+            upscale = false
+            merge = false
+"""
+
+
+def checkpointJson():
+    json_dict = {
+        "split video": False,
+        "split frames": False,
+        "clean": False,
+        "upscale": False,
+        "merge": False
+    }
+    return json_dict
+
+"""
+Split video into checkpoints
+Checkpoints make it easier to redo part of a restoration process.
+"""
+
+def splitvideo(file, main_json_file):
+    file_info = loadJSON(main_json_file)
+    checkpoints = file_info["checkpoints"]
+    file_duration = file_info["duration"]
+    checkpoint_duration = file_info["avg checkpoint duration"]
+    folder = file_info["folder"]
+    chk_start = 0
+    chk_end = checkpoint_duration
+    i = 0
+    while i < checkpoints:
+        checkpoint_number = str(i).zfill(3)
+        video = ffmpeg.input(file)
+        video = ffmpeg.trim(chk_start, chk_end)
+        video = ffmpeg.setpts()
+        video = ffmpeg.output(video, folder + "/checkpoints/" + checkpoint_number + file[-3:])
+        chk_start = chk_start + checkpoint_duration
+        chk_end = chk_end + checkpoint_duration
+        i = i + 1
+
+
+
+
+
+"""
+Split a Checkpoint into frames
+"""
+
+# Update Checkpoint JSON
+
+"""
+Perform Cleaning of frames
+"""
+
+# Update Checkpoint JSON
+
+"""
+Perform Upscaling of cleaned frames
+"""
+
+# Update Checkpoint JSON
+
+"""
+Merge frames into Checkpoint again
+"""
+
+# Update Checkpoint JSON
+
+"""
+Quality Control - Creat JSON of new Checkpoint file)
+"""
+
+# Compare new Checkpoint file with original JSON file
+
+# Update Checkpoint JSON
+
+# Update Main file JSON
